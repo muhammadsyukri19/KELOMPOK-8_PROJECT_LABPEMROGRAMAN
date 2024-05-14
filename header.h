1d109ef8248f6fe84cc3410e9fda4a07f27d7f21
Khairun Nisa <163709893+nisakhrn@users.noreply.github.com>
2024-05-14 21:30:46 +0700

    Add files via upload

---
+++ header.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Deklarasi struktur data untuk barang
+typedef struct Barang {
+  char nama[50];
+  int harga;
+  int stok;
+} Barang;
+
+// Deklarasi fungsi
+void menuUtama();
+void login();
+void tambahBarang();
+void hapusBarang();
+void lihatDaftarBarang();
+void lakukanPembayaran();
+void simpanDataKeFile();
+void bacaDataDariFile();
+
+// Deklarasi variabel global
+Barang barang[100]; // Array untuk menyimpan data barang
+int jumlahBarang = 0; // Jumlah barang yang terdaftar
+char namaPengguna[50]; // Nama pengguna yang sedang login
