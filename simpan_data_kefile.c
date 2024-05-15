87aad15cd65924b3212a51eedc737bc627b32930
Khairun Nisa <163709893+nisakhrn@users.noreply.github.com>
2024-05-15 23:02:35 +0700

    simpan_data_kefile.c

---
+++ simpan_data_kefile.c
@@ -0,0 +1,20 @@
+#include "header.h"
+void simpanDataKeFile() {
+    FILE *fp = fopen("data.txt", "w"); // Buka file dalam mode penulisan (w)
+
+    if (fp == NULL) {
+        printf("\nError: Gagal membuka file untuk menyimpan data.\n");
+        return;
+    }
+
+    // Simpan jumlah barang
+    fprintf(fp, "%d\n", jumlahBarang);
+
+    // Simpan data barang per baris
+    for (int i = 0; i < jumlahBarang; i++) {
+        fprintf(fp, "%s %d %d\n", barang[i].nama, barang[i].harga, barang[i].stok);
+    }
+
+    fclose(fp); // Tutup file
+    printf("\nData barang telah disimpan ke file dengan sukses.\n");
+}
