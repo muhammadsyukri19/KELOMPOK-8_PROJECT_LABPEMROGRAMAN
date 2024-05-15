402d2f87c3a2358e1dd667cd8b1421ce07db0872
Khairun Nisa <163709893+nisakhrn@users.noreply.github.com>
2024-05-15 23:17:18 +0700

    hapus_barang.c

--- hapus_barang.c
+++ hapus_barang.c
@@ -34,23 +34,3 @@ void hapusBarang() {
     printf("\nPilihan tidak valid. Silakan masukkan nomor barang yang benar.\n");
   }
 }
-
-
-  int pilihanHapus;
-  printf("Pilih nomor barang yang ingin dihapus: ");
-  scanf("%d", &pilihanHapus);
-
-  if (pilihanHapus > 0 && pilihanHapus <= jumlahBarang) {
-    // Pindahkan elemen array setelah elemen yang dihapus
-    for (int i = pilihanHapus - 1; i < jumlahBarang - 1; i++) {
-      barang[i] = barang[i + 1];
-    }
-
-    jumlahBarang--;
-    printf("\nBarang telah dihapus dengan sukses!\n");
-
-    simpanDataKeFile(); // Simpan perubahan data ke file
-  } else {
-    printf("\nPilihan tidak valid. Silakan masukkan nomor barang yang benar.\n");
-  }
-}
